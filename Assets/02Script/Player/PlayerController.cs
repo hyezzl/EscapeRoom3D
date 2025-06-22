12d8cd6f23f94e636b54a46b1e9046b53b865430
hyezzl <xia2ue@gmail.com>
2025-06-22 18:05:50 +0900

    인벤토리창 + Run 구현
    뛰는(shift) 기능 구현
    
    인벤토리창(UI)작업

--- Assets/02Script/Player/PlayerController.cs
+++ Assets/02Script/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Basic Setting")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float dashRatio = 2.5f;
 
     private CharacterController controller;
     private IInputHandler inputHandler;
@@ -26,13 +27,23 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
     }
 
+    // 플레이어 이동
     private void HandleMovement() {
         Vector2 dir = inputHandler.GetMovement();
+        bool isRunning = inputHandler.Run();
         Vector3 keyboarDir = new Vector3(dir.x, 0f, dir.y).normalized;
 
         Vector3 moveDir = Camera.main.transform.TransformDirection(keyboarDir).normalized;
         moveDir.y = 0;
 
-        controller.Move(moveDir * (moveSpeed * Time.deltaTime));
+        if (!isRunning)
+        {
+            controller.Move(moveDir * (moveSpeed * Time.deltaTime));
+        }
+        else {
+            controller.Move(moveDir * ((moveSpeed * dashRatio) * Time.deltaTime));
+        }
     }
+
+    
 }
