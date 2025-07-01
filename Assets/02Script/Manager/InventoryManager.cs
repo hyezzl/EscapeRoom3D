ee8aaeacf1d8695add207a3941436dec2abc2b3d
hyezzl <xia2ue@gmail.com>
2025-07-01 19:53:04 +0900

    윤곽선 작업1

--- Assets/02Script/Manager/InventoryManager.cs
+++ Assets/02Script/Manager/InventoryManager.cs
@@ -7,6 +7,18 @@ public class InventoryManager : Singleton<InventoryManager>
     private List<ItemInstance> inventory = new();
 
     public void AddItem(int itemID) { 
-        //var itemData = ItemDatabaseManager.Instance.GetData(itemID);
+        var itemData = ItemDatabaseManager.Instance.GetData(itemID);
+        if (itemData == null) {
+            Debug.Log($"{itemID} : unknown item ERROR");
+            return;
+        }
+        inventory.Add(new ItemInstance(itemID));
+    }
+
+    public IReadOnlyList<ItemInstance> GetInventory() => inventory.AsReadOnly();
+
+    // 세이브된 인벤토리 데이터 로드
+    public void LoadItemInstance(ItemInstance newItemInst) { 
+        inventory.Add(newItemInst);
     }
 }
