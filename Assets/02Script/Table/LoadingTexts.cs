66ffea32e470bf8d678023e1eb7cb71b5efe44e8
hyezzl <xia2ue@gmail.com>
2026-03-08 04:32:50 +0900

    오류 수정

--- Assets/02Script/Table/LoadingTexts.cs
+++ Assets/02Script/Table/LoadingTexts.cs
@@ -2,16 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExcelAsset(AssetPath = "Resources/Table")]
-//public class LoadingTexts : ScriptableObject
-//{
-//	public List<LoadingTextsEntity> loadingTexts; // Replace 'EntityType' to an actual type that is serializable.
-//}
-
-[System.Serializable]
-public class LoadingTexts
+public class LoadingTexts : ScriptableObject
 {
-    public List<LoadingTextsEntity> loadingTexts;
+	public List<LoadingTextsEntity> loadingTexts; // Replace 'EntityType' to an actual type that is serializable.
 }
+
+//[System.Serializable]
+//public class LoadingTexts
+//{
+//    public List<LoadingTextsEntity> loadingTexts;
+//}
 
 [System.Serializable]
 public class LoadingTextsEntity
